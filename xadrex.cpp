8c4a6bd41c70e44659f6424cf4c4f79ec88d2eb3
jrlimax <juninho_lima2020@hotmail.com>
2023-03-28 09:28:47 -0300

    Tabuleiro pronto, atualizando movimento dos peões.

---
+++ xadrex.cpp
@@ -0,0 +1,75 @@
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+
+  char tabuleiro[9][9]={
+
+    ' ', '1', '2', '3', '4', '5', '6', '7', '8',
+    '1', 'T' , 'C', 'B', 'Q', 'R', 'B', 'C', 'T',
+    '2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 
+    '3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    '4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    '5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    '6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    '7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 
+    '8', 'T', 'C', 'B', 'Q', 'R', 'B', 'C', 'T',
+  };
+
+void colocarTabuleiro(){
+
+  for (int linha = 0; linha < 9; linha++){
+    std::cout << std::endl;
+    for (int coluna = 0; coluna < 9; coluna++){
+    std::cout << tabuleiro[linha][coluna];
+    if (linha == 0){
+        std:: cout << "   ";
+    }else { 
+      std::cout << " | ";
+      }
+    }
+  }
+  
+  std::cout << std::endl;
+
+}
+
+void movimentarPeao(int linhaPeca, int colunaPeca){
+    
+    int destinoLinha;
+    int destinoColuna;
+
+    std::cout << "Digite a linha destino: ";
+    std::cin >> destinoLinha;
+
+    std::cout << "Digite a coluna destino: ";
+    std::cin >> destinoColuna;
+
+      if (tabuleiro[destinoLinha][destinoColuna] == ' ') {
+        tabuleiro[destinoLinha][destinoColuna] = 'P';
+        tabuleiro[linhaPeca][colunaPeca] = ' ';
+      }
+
+}
+
+int main(){
+
+  int linhaPeca;
+  int colunaPeca;
+
+  colocarTabuleiro();
+
+  std::cout << std::endl;
+
+  std::cout << "Digite a linha da peça: ";
+  std::cin >> linhaPeca;
+
+  std::cout << "Digite a coluna da peça: ";
+  std::cin >> colunaPeca;
+
+  if(tabuleiro[linhaPeca][colunaPeca] == 'P'){    
+    movimentarPeao(linhaPeca, colunaPeca);   
+  }
+
+  colocarTabuleiro();
+
+}
